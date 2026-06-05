a3f3ccda3dc754565a1b2cc05b48d7372c3c90c4
Adam <adam@repfinder.local>
2026-06-05 12:09:25 +0200

    Fix SessionProvider async layout compatibility

--- src/components/SessionProviderWrapper.jsx
+++ src/components/SessionProviderWrapper.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
 
-export default function SessionProviderWrapper({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function SessionProviderWrapper({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
